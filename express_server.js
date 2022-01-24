7bd0ae6a49d71ea08122d1520d235b314160c80d
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-01-24 18:28:20 -0500

    get the randomly 6 length shortURL function

--- express_server.js
+++ express_server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
+
 const app = express();
 const port = 8080;
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+}
+
+function generateRandomString() {
+  let strPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    + "0123456789"
+    + "abcdefghijklmnopqrstuvxyz";
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    let index = parseInt(strPool.length * Math.random());
+    result += strPool.charAt(index);
+  }
+  return result;
 }
 
 app.get('/', (req, res) => {
@@ -26,9 +42,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
+})
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('ok');
 })
 
 app.listen(port, () => {
